aaef476a92808b1192d995ee5f8dd3fb4ec165af
sabbirsam <samiubat14@gmail.com>
2023-01-23 11:54:17 +0600

    Complete design part

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    // owner
+    <div className='caf_message'>
+      <div className="caf_messageInfo">
+        <img src="https://images.pexels.com/photos/3968442/pexels-photo-3968442.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <span>Just now</span>
+      </div>
+      <div className="caf_messageContent">
+        <p>Hello there! How are you today?</p>
+        <img src="https://images.pexels.com/photos/3968442/pexels-photo-3968442.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+      </div>
+    </div>
   )
 }
 
